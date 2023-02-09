6934d22557fa5e6d6c6392545c9bcd329caf1026
Rodrigo-Tei <rodrigoteidalmas@gmail.com>
2023-02-09 19:16:11 -0300

    Add handle to pokemon strings including name and types

--- lib/modules/pokedex/bloc/pokemon_list_bloc.dart
+++ lib/modules/pokedex/bloc/pokemon_list_bloc.dart
@@ -13,15 +13,26 @@ class PokemonListBloc extends Bloc<PokemonListEvent, PokemonListState> {
     return word[0].toUpperCase() + word.substring(1);
   }
 
+  String splitPokemonName(String name) {
+    return name.split('-')[0];
+  }
+
+  void handlePokemonStrings(Pokemon pokemon) {
+    pokemon.name = capitalizeFirstLetter(pokemon.name);
+    if (pokemon.name.contains('-')) {
+      pokemon.name = splitPokemonName(pokemon.name);
+    }
+    for (int i = 0; i < pokemon.types.length; i++) {
+      pokemon.types[i] = capitalizeFirstLetter(pokemon.types[i]);
+    }
+  }
+
   PokemonListBloc(this.pokemonRepository) : super(PokemonListLoading()) {
     on<FetchPokemonList>((event, emit) async {
       emit(PokemonListLoading());
       _pokemonList = await pokemonRepository.getUserPokemons();
       for (Pokemon pokemon in _pokemonList) {
-        pokemon.name = capitalizeFirstLetter(pokemon.name);
-        for (int i = 0; i < pokemon.types.length; i++) {
-          pokemon.types[i] = capitalizeFirstLetter(pokemon.types[i]);
-        }
+        handlePokemonStrings(pokemon);
       }
       emit(PokemonListLoaded(_pokemonList));
     });
