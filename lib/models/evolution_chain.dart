01b86d628580c4a955c4c0b6a1706934e6f9536a
Rodrigo-Tei <rodrigoteidalmas@gmail.com>
2023-07-07 16:36:17 -0300

    Update dependencies

--- lib/models/evolution_chain.dart
+++ lib/models/evolution_chain.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:pokedex/models/evolution_details.dart';
 import 'package:pokedex/models/pokemon.dart';
